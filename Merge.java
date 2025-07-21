2fb476efe1a735671d76b873fee361d19b5081a9
Khamkar Suraj <135463570+surajkhamkar06@users.noreply.github.com>
2025-07-22 01:39:05 +0530

    Merge String using concat mathod

---
+++ Merge.java
@@ -0,0 +1,10 @@
+class Merge
+{
+	public static void main(String[] args) 
+	{
+		String str1 = "Suraj";
+		String str2 = "Khamkar";
+		String str3 = str1.concat(" " + str2);
+		System.out.println(str3);		
+	}
+}
